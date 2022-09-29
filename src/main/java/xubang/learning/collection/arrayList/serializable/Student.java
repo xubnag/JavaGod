8b4d6dddb968aa50e4fa7524c27e5fc09aa61136
xubang <3307629938@qq.com>
2022-09-29 22:51:38 +0800

    StringBuilder  优化 字符串拼接

--- src/main/java/xubang/learning/collection/arrayList/serializable/Student.java
+++ src/main/java/xubang/learning/collection/arrayList/serializable/Student.java
@@ -20,10 +20,26 @@ public class Student implements Serializable {
         this.age = age;
     }
 
+
+    //字符串拼接会产生不必要的对象，浪费空间   --->  StringBuilder()
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
+
     @Override
     public String toString() {
-        return "I am " +
-                 name  +
-                ", my age is " + age;
+        StringBuilder builder = new StringBuilder();
+        builder.append("Student{ name= ");
+        builder.append(this.name);
+        builder.append(" , age = ");
+        builder.append(this.age);
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
